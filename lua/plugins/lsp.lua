c0fe5ae0cd38efcf97a3be1edb342b9deb5a73ad
Eliass-il <eliassmat.il@gmail.com>
2025-06-03 19:25:07 +0200

    Added lua_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,13 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "jdtls", "pyright", "sqls", "jsonls", "html", "cssls" },
+    ensure_installed = { "jdtls", "pyright", "sqls", "jsonls", "html", "cssls", "lua_ls" },
     automatic_installation = true,
 })
 
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+lspconfig.lua_ls.setup { capabilities = capabilities }
 lspconfig.pyright.setup { capabilities = capabilities }
 lspconfig.sqls.setup { capabilities = capabilities }
 lspconfig.html.setup { capabilities = capabilities }
